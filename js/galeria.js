5c9b333ce34cfa369aaa2ea096e71eb73d1088af
Jose <jrfo_04@unitec.edu>
2018-02-11 18:38:57 -0600

    Eliminar cambios

--- js/galeria.js
+++ js/galeria.js
@@ -1,58 +1,3 @@
-var num = 1;
-var aux = 0;
-/*
-function Adelante() {
-	num++;
-	aux = num;
-
-	if (num > 15) {
-		num = 1;
-	}//Fin del if.
-	var foto1 = document.getElementById("Visor1");
-	foto1.src = "./img/foto"+num+".jpg";
-	num++;
-	if (num > 15) {
-		num = 1;
-	}//Fin del if.
-	var foto2 = document.getElementById("Visor2");
-	foto2.src = "./img/foto"+num+".jpg";
-	num++;
-	if (num > 15) {
-		num = 1;
-	}//Fin del if.
-	var foto3 = document.getElementById("Visor3");
-	foto3.src = "./img/foto"+num+".jpg";
-	num++;
-	if (num > 15) {
-		num = 1;
-	}//Fin del if.
-	var foto4 = document.getElementById("Visor4");
-	foto4.src = "./img/foto"+num+".jpg";
-	num++;
-	if (num > 15) {
-		num = 1;
-	}//Fin del if.
-	var foto5 = document.getElementById("Visor5");
-	foto5.src = "./img/foto"+num+".jpg";
-
-	if (num > 4) {
-		num = num - 4;
-	} else if (num == 1) {
-		num = aux;
-	} else if (num == 2 || num == 3 || num == 4) {
-		num = aux + 1;
-	}
-}//Fin de la funcion Adelante().
-
-function Atras() {
-	num--;
-	if (num < 1) {
-		num = 15;
-	}//Fin del if.
-	var foto = document.getElementById("foto");
-	foto.src="./img/foto"+num+".jpg";
-}//Fin de la funcion Atras().
-*/
 var imgArray = new Array("foto1.jpg","foto2.jpg","foto3.jpg","foto4.jpg","foto5.jpg","foto6.jpg","foto7.jpg"
 ,"foto8.jpg","foto9.jpg","foto10.jpg","foto11.jpg","foto12.jpg","foto13.jpg","foto14.jpg","foto15.jpg");
 
@@ -106,5 +51,5 @@ function f_slideshow( xflip ) {
 		document.images[4].src =  "./img/" + imgArray[curImage + 3];
 		document.images[5].src =  "./img/" + imgArray[curImage + 1];
 	}//Fin del if else
-	
+
 }//Fin de la funcion
